81c74f8a0e2c45d953a5db4fd2210aaf074a9116
anapsousa <anapsousa@gmail.com>
2025-12-22 20:53:15 +0000

    deleting migrations which are unnecessary

--- supabase/migrations/20251217111520_6c52d20f-4e84-49e2-9faa-256d36d3ec81.sql
+++
@@ -1,2 +0,0 @@
--- Make the design-files bucket public so product images can be displayed
-UPDATE storage.buckets SET public = true WHERE id = 'design-files';
--- supabase/migrations/20251219105642_c0bb4111-5a55-4d1f-bc77-87a66e2b06b9.sql
+++
@@ -1,2 +0,0 @@
--- Add image_url column to calc_prints for storing print photos
-ALTER TABLE public.calc_prints ADD COLUMN IF NOT EXISTS image_url text;
